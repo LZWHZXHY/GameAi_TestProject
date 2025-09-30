12507b730e2de9c4c30e294743311aaea93a3020
LZWHZXHY <LZWHZXHY@gmail.com>
2025-09-30 19:19:26 -0400

    UPDATE TEST

--- steering-java-cars-part1/steering-java-cars-part1/src/controllers/Vector2D.java
+++ steering-java-cars-part1/steering-java-cars-part1/src/controllers/Vector2D.java
@@ -15,7 +15,10 @@ public class Vector2D {
         return new Vector2D(this.x+other.x, this.y+other.y);
     }
 
-
+    //Vector 向量长度计算
+    public double magnitude(){
+        return Math.sqrt(x*x + y*y);
+    }
 
 
     public static void main(String[] args){
@@ -24,6 +27,10 @@ public class Vector2D {
         // 测试1：创建向量
         Vector2D v = new Vector2D(3, 4);
         System.out.println("创建向量: (" + v.x + ", " + v.y + ")");
+        // 测试2：计算向量长度
+        double length = v.magnitude();
+        System.out.println("向量长度: " + length);
+
     }
 
 
